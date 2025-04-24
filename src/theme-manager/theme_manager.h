2c7b644d7abc1b44c97cc66b73347677ba80fa5d
アクゼスティア <akzestia@gmail.com>
2025-04-24 05:53:51 +0200

    Theme provider, manages themes and provides selected theme properties to QML

---
+++ src/theme-manager/theme_manager.h
@@ -0,0 +1,16 @@
+#ifndef THEME_MANAGER_X
+#define THEME_MANAGER_X
+
+#include "qobject.h"
+#include "qtmetamacros.h"
+
+class ThemeManager : public QObject {
+
+    Q_OBJECT
+
+    public:
+
+
+};
+
+#endif // THEME_MANAGER_X
