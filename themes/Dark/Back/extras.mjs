a33d278c4a5aaa79ec6521678eeafd58a564a6ce
アクゼスティア <akzestia@gmail.com>
2025-04-24 05:50:49 +0200

    1st template version for themes

---
+++ themes/Dark/Back/extras.mjs
@@ -0,0 +1,18 @@
+/*
+    Extras
+
+    Your js logic in1 file ^_^
+*/
+
+/*
+    Provides context for "Kawaii_loader"
+
+    Basicly pathes to your desired Qml View used when calling
+    Kawai_loader.change_view(const QString& view);
+*/
+export function provide_loader_context() {
+    return {
+        Auth: "Front/Auth.qml",
+        Menu: "Front/Menu.qml",
+    };
+}
